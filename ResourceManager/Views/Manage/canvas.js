8da5ceaae939bb622bd5714f1299bfe2a9eeb202
lichal <32421669+lichal@users.noreply.github.com>
2018-11-05 19:32:45 -0500

    Commit all

---
+++ ResourceManager/Views/Manage/canvas.js
@@ -0,0 +1,14 @@
+﻿window.onload = function(){
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext("2d");
+
+
+
+    var c = $('#' + '@(ViewBag.ProcessNumber)').val();
+
+
+        ctx.fillText(c,10,90);
+        ctx.fillStyle = 'Green';
+        ctx.fillRect(200*i, 0, 100, 100);
+    
+}
